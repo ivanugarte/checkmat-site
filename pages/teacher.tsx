319eda83d8f5915e7b678d45ec507b523f84794e
ivanugarte <ivanugartem@gmail.com>
2022-09-04 17:40:13 -0300

    teachers

--- pages/teacher.tsx
+++ pages/teacher.tsx
@@ -117,7 +117,7 @@ export default function Teacher() {
                   className="transition-all transform translate-y-8 opacity-0  group-hover:opacity-100 group-hover:translate-y-0"
                 >
                   <strong>    <p className="text-sm text-black text-center font-mono text-2xl">
-                    Profesor Ghilerme Rimeiro Faixa preta en Brazilian Jiu-jitsu.
+                    Profesor Guilherme Ribeiro Faixa preta en Brazilian Jiu-jitsu.
                   </p></strong>
                 </div>
               </div>
@@ -149,13 +149,14 @@ export default function Teacher() {
 
           <Image
             className="object-cover w-full h-56 sm:h-full"
-            src="/img/2g_01.jpg"
+            src="/img/2g_03.jpg"
             layout='fill'
             objectFit="contain"
             alt=""
           />
         </aside>
         <PhotoGalery />
+        <Banner  />
         {/* <Video1 />
         <Video2 /> */}
       </Layout>
@@ -226,5 +227,44 @@ function PhotoGalery() {
         </div>
       </div>
     </div>
+  )
+}
+
+function Banner() {
+  return (
+    <section className="relative bg-white">
+      <img
+        className="absolute inset-0 object-[75%] sm:object-[25%] object-cover w-full h-full opacity-25 sm:opacity-100"
+        src="/img/2g_03.jpg"
+        alt="Couple on a bed with a dog"
+      />
+
+      <div className="hidden sm:block sm:inset-0 sm:absolute sm:bg-gradient-to-r sm:from-white sm:to-transparent"></div>
+
+      <div className="relative px-4 py-32 mx-auto max-w-screen-xl lg:h-screen lg:items-center lg:flex">
+        <div className="max-w-xl text-center sm:text-left">
+          <h1 className="text-3xl font-extrabold sm:text-5xl">
+            Let us find your
+            <strong className="font-extrabold text-rose-700 sm:block">
+              Forever Home.
+            </strong>
+          </h1>
+
+          <p className="max-w-lg mt-4 sm:leading-relaxed sm:text-xl">
+            Lorem ipsum dolor sit amet consectetur, adipisicing elit. Nesciunt illo tenetur fuga ducimus numquam ea!
+          </p>
+
+          <div className="flex flex-wrap mt-8 text-center gap-4">
+            <a className="block w-full px-12 py-3 text-sm font-medium text-white rounded shadow bg-rose-600 sm:w-auto active:bg-rose-500 hover:bg-rose-700 focus:outline-none focus:ring" href="/get-started">
+              Get Started
+            </a>
+
+            <a className="block w-full px-12 py-3 text-sm font-medium bg-white rounded shadow text-rose-600 sm:w-auto hover:text-rose-700 active:text-rose-500 focus:outline-none focus:ring" href="/about">
+              Learn More
+            </a>
+          </div>
+        </div>
+      </div>
+    </section>
   )
 }
